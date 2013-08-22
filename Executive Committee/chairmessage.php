6466edd4b248f73e0fca84b9b35210978c4e5121
Mark Wolf <mark.wolf.music@gmail.com>
2013-08-22 16:28:51 -0400

    Upgraded to bootstrap 3 and added javascript picture slider.

---
+++ Executive Committee/chairmessage.php
@@ -0,0 +1,18 @@
+<?php
+include "../header.php";
+include "../menu.php";
+?>
+
+<td>
+  <h3>Message from the KACS Chair</h3>
+  <p>Dear Readers,</p>
+  <p>I thank all the volunteers who helped make recent KACS Education events a success: the <em>High School Chemistry Symposium</em> on April 30 and the annual <em>Awards Ceremony</em> on May 20. A special thank you goes to Dr. James Kiddle (WMU), the KACS Chair of the Education Subcommittee in charge of identifying outstanding High School chemistry students through the competitive exams process.</p>
+
+  <p>There is a special call for volunteers for <strong><em>Chemistry Day at the Museum</em></strong> scheduled for October 12. <strong>We need you!</strong> Please contact Dr. Lydia Hines (<a href="mailto:lemhwgh@gmail.com?subject=Chemistry Day at the Museum">lemhwgh@gmail.com</a>) <strong>as soon as possible</strong>. KACS also needs to identify officers for our upcoming joint regional meeting in 2015 as outlined on page 2 of the July newsletter.</p>
+  <p>Also find out why you should mark your calendar for a <strong>party on Tuesday, September 10</strong> at the Westin Indianapolis Hotel.</p>
+  <p>We hope that you will continue to check in with us to learn about the events we are planning for our fall schedule.</p>
+  July 11, 2013</p>
+
+</td>
+
+<?php include "../footer.php" ?>
---
+++ menu.php
@@ -0,0 +1,79 @@
+      <tr height="0">
+	<td width="251" rowspan="2" valign="top" bgcolor="#FFCC33">
+          <dl>
+	    <dt>
+	      <font size="2" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">
+		<strong><br />
+		  News
+		</strong>
+	      </font>
+            <dt>
+	      <a href="kalchem.htm">
+		<font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">
+		  Newsletter
+		</font>
+	      </a>
+	    </dt>
+       	    <dt>
+	      <font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">
+		<a href="Activities/pastevents.htm">
+		   Past Events
+		</a>
+	      </font>
+	    </dt>
+            <dt>
+	      <a href="Jobs/Jobs.html"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Job opportunities</font></a>
+	    </dt>
+	    <dt>
+	      <font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">
+		<a href="History/history.htm"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">KACS History</font>
+		</a>
+	      </font>
+	    </dt>
+            <dt><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif"><a href="History/memorial.html">Memorials</a></font><a href="History/memorial.html"></a>
+	    </dt>
+	  </dl>
+	  <dl>
+	    <dt><font size="2" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif"><strong>About Us<br>
+	      </strong></font>
+	    </dt>
+	    <dt><font size="2" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif"><a href="Executive Committee/chairmessage.php">Message from the Chair</a></font></dt>
+            <dt><font size="2" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif"><a href="Executive Committee/KACSoffice.html">Executive Committee</a></font></dt>
+	    <dt><a href="Executive Committee/ECminutes.htm"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">EC Meeting Minutes</font></a></dt>
+            <dt><a href="Executive Committee/KACS_bylaws_120709.pdf"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Bylaws</font></a></dt>
+            <dt><a href="Executive Committee/Operations Manual/KACS Operations Manual 03-14-12.pdf"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Operations Manual</font></a><a href="Executive Committee/KACS Operations Manual 02-05-11.pdf"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif"></font></a></dt>
+            <dt><a href="Executive Committee/AnnualReports.htm"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Annual Reports</font></a></dt>
+            <dt><a href="Reports/councilor.htm"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Councilor Reports</font></a></dt>
+            <dt><a href="Executive Committee/2013/Volunteer Opportunities Jan 2013.pdf"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Volunteer Opportunities</font></a></dt>
+            <dt><a href="History/kacs1981.pdf"></a></dt>
+	  </dl>
+
+          <dl>
+	    <dt>
+	      <b>
+		<font size="2" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">
+		  Student Chapter Activities
+		</font>
+	      </b>
+	    </dt>
+	    <dt><a href="http://wmich.collegiatelink.net/organization/chemistry-graduate-student-association-at-wmu"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">WMU Chem. Grad. Student Assoc.</font></a></dt>
+            <dt><a href="http://www.facebook.com/group.php?gid=476178335071"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">WMU Chem. Club</font></a></dt>
+            <dt><a href="http://www.facebook.com/groups/343451499029909/"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Kalamazoo College Chem. Club</font></a></dt>
+          </dl>
+          <dl>
+	    <dt><b><font size="2" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Other Links</font></b></dt>
+	    <dt><a href="http://www.presentations.acs.org/"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">ACS Presentations on Demand</font></a></dt>
+            <dt><a href="http://www.softconference.com/ACSchem/"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Content from recent ACS Meetings</font></a></dt>
+	    <dt><a href="http://dgr.rints.com/"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Directory of Graduate Research</font></a></dt>
+            <dt><a href="http://portal.acs.org:80/portal/PublicWebSite/careers/index.htm"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Career Services</font></a></dt>
+            <dt><a href="http://portal.acs.org/portal/PublicWebSite/pressroom/index.htm"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Articles from ACS National</font></a></dt>
+            <dt><a href="http://vimeo.com/21063971"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">The ACS Staff: How Does it Support Governance?</font></a></dt>
+            <dt><a href="Activities/Kzoo_Links.htm"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Kalamazoo Links</font></a></dt>
+            <dt><a href="http://www.cas.org/"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Chemical Abstracts Service</font></a></dt>
+            <dt><a href="http://www.chem.ucla.edu/chem_news.html"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Chemistry News Groups</font></a></dt>
+            <dt><a href="http://e-skolar.com/docs/educational-chemistry-resources.php"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Chemistry Educational Resources</font></a></dt>
+            <dt><a href="http://safety.dow.com/"><font size="-1" face="Helvetica, Geneva, Arial, SunSans-Regular, sans-serif">Go to the ACSWeb<br>
+		  Safety Advice by Dow</font></a><br>
+            </dt>
+	  </dl>
+	</td>
